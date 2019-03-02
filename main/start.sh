4ac9efcd2c229d10d20908355777b4d89f668297
Uttam Khanal <uttam_khanal12@hotmail.com>
2019-03-02 17:21:53 +0545

    add all files

---
+++ main/start.sh
@@ -0,0 +1,17 @@
+#wait for mysql
+#while true; do
+#	echo Connecting to SQL
+#	mysql -h mariadb --database=ttar
+#	if [ $? -eq 0 ]; then
+#		break
+#	fi
+#	sleep 5
+#done
+
+#launch websocket server
+cd /typingtarzan
+php bin/chat-server.php &
+
+#launch cake web server
+cd webroot
+../bin/cake server -H 0.0.0.0 -p 8080
